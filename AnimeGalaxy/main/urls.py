47986fd305b99fe4986f2f98e16d0e2c466df913
S4YG4 <telemovelsic@hotmail.com>
2019-05-30 11:53:11 +0100

    Added API Endpoints for current Serializers

--- AnimeGalaxy/main/urls.py
+++ AnimeGalaxy/main/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
+	path('episodes', views.EpisodesView.as_view({"get": "list", "post": "create"}), name='list-episodes'),
+	path('videos', views.VideoView.as_view({"post": "create"}), name='list-videos'),
+	path('videos/<int:pk>', views.EpisodesView.as_view({"get": "retrieve"}), name='retrieve-episode'),
+	path('playlist/<int:pk>', views.UrlView.as_view({"get": "retrieve"}), name='retrieve-urls'),
 ]
--- AnimeGalaxy/main/views.py
+++ AnimeGalaxy/main/views.py
@@ -1,5 +1,83 @@
-from django.http import HttpResponse
+from typing import List
+
+from rest_framework import status, viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import AllowAny, BasePermission
+from rest_framework.response import Response
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+
+from .models import Episode, Video
+from .serializers import EpisodeCreateSerializer, EpisodeSerializer, PlaylistSerializer, VideoCreateSerializer
 
 
-def index(request):
-	return HttpResponse("Hello, world")
+class BaseMVS(viewsets.ModelViewSet):
+	# View Permissions
+	permission_classes: List[BasePermission] = [AllowAny]
+	throttle_classes = [UserRateThrottle, AnonRateThrottle]
+
+	# Serializers
+	create_serializer = list_serializer = update_serializer = retrieve_serializer = None
+
+	# Use Different Serializer For Create
+	def create(self, request, *args, **kwargs):
+		self.serializer_class = self.create_serializer or self.serializer_class
+		return super(BaseMVS, self).create(request, *args, **kwargs)
+
+	# Use Different Serializer For Retrieve
+	def retrieve(self, request, *args, **kwargs):
+		self.serializer_class = self.retrieve_serializer or self.serializer_class
+		return super(BaseMVS, self).create(request, *args, **kwargs)
+
+	# Use Different Serializer For List
+	def list(self, request, *args, **kwargs):
+		self.serializer_class = self.list_serializer or self.serializer_class
+		return super(BaseMVS, self).list(request, *args, **kwargs)
+
+	# Use Different Serializer For Update
+	def update(self, request, *args, **kwargs):
+		self.serializer_class = self.update_serializer or self.serializer_class
+		return super(BaseMVS, self).update(request, *args, **kwargs)
+
+	# Handle Validation Errors
+	def handle_exception(self, exc: Exception):
+		if isinstance(exc, ValidationError):
+			return Response({"error": {'message': exc.detail, 'code': 406}}, status.HTTP_406_NOT_ACCEPTABLE)
+
+		return super(BaseMVS, self).handle_exception(exc)
+
+
+class EpisodesView(BaseMVS):
+	# Query Options
+	queryset = Episode.objects.all().order_by("-pk")[:12]
+	serializer_class = EpisodeSerializer
+	create_serializer = EpisodeCreateSerializer
+
+	filterset_fields = ('id',)
+
+	def retrieve(self, request, pk=None, *args, **kwargs):
+		queryset = get_object_or_404(Episode, id=pk)
+		if not queryset:
+			return Response(status=status.HTTP_400_BAD_REQUEST)
+		else:
+			serializer = EpisodeSerializer(queryset, many=False)
+			return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class VideoView(BaseMVS):
+	create_serializer = VideoCreateSerializer
+
+
+class UrlView(BaseMVS):
+	# Query Options
+	queryset = Video.objects.all()
+	serializer_class = PlaylistSerializer
+
+	def retrieve(self, request, pk=None, *args, **kwargs):
+		requested_episode = Episode.objects.get(pk=pk)
+		if not requested_episode:
+			return Response(status=status.HTTP_404_NOT_FOUND)
+		else:
+			episodes = Episode.objects.filter(anime=requested_episode.anime, number__gte=requested_episode.number)
+			serializer = PlaylistSerializer(episodes, many=True, context={"request": request})
+			return Response(serializer.data, status=status.HTTP_200_OK)
