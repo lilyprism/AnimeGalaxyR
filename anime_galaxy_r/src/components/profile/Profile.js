3f822e6ee888b6bb122a7faee5ec1c26876ea7d4
INatsuz <vascoraminhos@hotmail.com>
2019-06-19 11:53:04 +0100

    +Added editability feature +Added user state variable

--- anime_galaxy_r/src/components/profile/Profile.js
+++ anime_galaxy_r/src/components/profile/Profile.js
@@ -1,35 +1,71 @@
 import React from 'react';
 
 import "./profile.sass";
+import RequestUtilities from "../../util/RequestUtilities";
+
+class ProfileDescription extends React.Component {
+
+    render() {
+        if (!this.props.is_desc_editable) {
+            return (
+                <p>
+                    {this.props.user !== null ? this.props.user.description : "A carregar a descrição do utilizador"}
+                </p>
+            );
+        } else {
+            return (
+                <textarea className="profile-header-description-textarea">
+                    {this.props.user !== null ? this.props.user.description : "A carregar a descrição do utilizador"}
+                </textarea>
+            );
+        }
+    }
+
+}
 
 export default class Profile extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            is_desc_editable: false,
+            is_img_editable: false,
+            user: null
+        };
+        this.getUser();
+    }
+
+    getUser = () => {
+        if (this.props.is_logged_in) {
+            RequestUtilities.sendGetRequest("auth/user", true).then(res => {
+                this.setState({user: res.data});
+            });
+        }
+    };
+
+    toggleEditable = () => {
+        this.setState({
+            is_desc_editable: !this.state.is_desc_editable,
+            is_img_editable: !this.state.is_img_editable
+        });
+    };
 
     render() {
         return (
             <div className="profile">
                 <div className="profile-header">
-                    <div className="profile-header-img">
-                        <img src={this.props.user !== null ? this.props.user.avatar : "http://via.placeholder.com/500"} alt="User Profile"/>
+                    <div className={`profile-header-img ${this.state.is_img_editable ? "editable" : ""}`}>
+                        <img className={`profile-img ${this.state.is_img_editable ? "editable" : ""}`} src={this.state.user !== null ? this.state.user.avatar : "http://via.placeholder.com/500"} alt="User Profile"/>
                     </div>
                     <div className="profile-header-content">
                         <div className="profile-header-content-top">
-                            <span className="profile-header-username">{this.props.user !== null ? this.props.user.username : "Username Here"}</span>
-                            <span className="profile-settings-btn cursor-pointer"><i className="fas fa-cog"/></span>
+                            <span className="profile-header-username">{this.state.user !== null ? this.state.user.username : "Username Here"}</span>
+                            <span className="profile-settings-btn cursor-pointer" onClick={this.toggleEditable}><i className="fas fa-cog"/></span>
                         </div>
                         <div className="profile-header-content-middle">
                             <div className="profile-header-description">
-                                <p>
-                                    This is the default description for a new user on the platform
-                                </p>
-                                <p>
-                                    This way, I can actually check what the profile page would look like with an actual user.
-                                    Like, I need to check what the page looks like with a description of realistic size, and this
-                                    text server that purpose by filling the space where the user description would be.
-                                </p>
-                                <p>
-                                    What I mean is, this is placeholder information to facilitate
-                                    the testing phase of the page development.
-                                </p>
+                                <ProfileDescription is_desc_editable={this.state.is_desc_editable} user={this.state.user}/>
                             </div>
                         </div>
                         <div className="profile-header-content-bottom">
