e0ee75a9cd035e05d6b2df660b363d6dcfcd1ecc
INatsuz <vascoraminhos@hotmail.com>
2019-06-21 10:32:42 +0100

    ~Changed AnimeList component to EpisodeHistoryList component

---
+++ anime_galaxy_r/src/components/profile/EpisodeHistoryList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import "./episodehistorylist.sass";
+
+export default class EpisodeHistoryList extends React.Component {
+
+    render() {
+        return (
+            <div className="episodehistorylist-container">
+                <ul className="episodehistorylist">
+                    {this.props.items.map(function (item, index) {
+                        return <li className="episodehistorylist-item">{`${item.episode.anime.name} - ${item.episode.number}`}</li>
+                    })}
+                </ul>
+            </div>
+        );
+    }
+
+};
