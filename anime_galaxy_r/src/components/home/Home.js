51a8d2ebc053f640d8257f4bb766a74381dd1efb
INatsuz <vascoraminhos@hotmail.com>
2019-06-24 13:09:44 +0100

    ~Changing the homepage to the new one

--- anime_galaxy_r/src/components/home/Home.js
+++ anime_galaxy_r/src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import CardLayout from "./../CardLayout";
-import {Carousel} from "./Carousel";
+import {OldCarousel} from "./OldCarousel";
 import RequestUtilities from "../../util/RequestUtilities";
 
 export default class Home extends React.Component {
@@ -61,21 +61,51 @@ export default class Home extends React.Component {
     // }
 
     render() {
-        return (
-            <div className="breakpoint-container">
-                <h1 className="title"><span><i className="fas fa-newspaper"/>  Episódios:</span></h1>
-                <CardLayout card_type={"LatestEpisodeCard"} items={this.state.latest_episodes}/>
-                <h1 className="title"><span><i className="fas fa-newspaper"/>  Últimos Animes:</span></h1>
-                <Carousel item_type="CarouselItem" items={this.state.latest_anime}/>
-                <h1 className="title"><span><i className="fas fa-newspaper"/>  Assistidos No Momento:</span></h1>
-                <Carousel item_type="CarouselItemEpisode" items={this.state.watched_anime}/>
-            </div>
-        );
         // return (
         //     <div className="breakpoint-container">
-        //
+        //         <h1 className="title"><span><i className="fas fa-newspaper"/>  Episódios:</span></h1>
+        //         <CardLayout card_type={"LatestEpisodeCard"} items={this.state.latest_episodes}/>
+        //         <h1 className="title"><span><i className="fas fa-newspaper"/>  Últimos Animes:</span></h1>
+        //         <OldCarousel item_type="CarouselItem" items={this.state.latest_anime}/>
+        //         <h1 className="title"><span><i className="fas fa-newspaper"/>  Assistidos No Momento:</span></h1>
+        //         <OldCarousel item_type="CarouselItemEpisode" items={this.state.watched_anime}/>
         //     </div>
         // );
+        return (
+            <div>
+
+                <div className="border-bottom-red"/>
+                <div className="gradient-container">
+                    <div className="breakpoint-container">
+                        <h2 className="bg-title">
+                            <div className="pless-title">
+                                <span>Últimos Episódios</span>
+                            </div>
+                        </h2>
+                        <div className="spacer"/>
+                        <div className="spacer"/>
+                        <div className="spacer"/>
+                        <div className="spacer"/>
+                        <div className="spacer"/>
+                    </div>
+                </div>
+                <div className="border-bottom-red"/>
+                <div className="gradient-container">
+                    <div className="breakpoint-container">
+                        <h2 className="bg-title">
+                            <div className="pless-title">
+                                <span>Últimos Animes</span>
+                            </div>
+                        </h2>
+                        <div className="spacer"/>
+                        <div className="spacer"/>
+                        <div className="spacer"/>
+                        <div className="spacer"/>
+                        <div className="spacer"/>
+                    </div>
+                </div>
+            </div>
+        );
     }
 
 };
