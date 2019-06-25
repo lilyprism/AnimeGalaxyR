12a9943b29fbd07cfc50af274cea7275c04e4984
INatsuz <vascoraminhos@hotmail.com>
2019-06-25 10:24:47 +0100

    >Finished Carousel (for now at least)

--- anime_galaxy_r/src/components/home/Carousel.js
+++ anime_galaxy_r/src/components/home/Carousel.js
@@ -10,43 +10,79 @@ export default class Carousel extends React.Component {
         this.state = {
             items: [
                 {
-                    image: "http://via.placeholder.com/1920x750?text=1"
+                    image: "http://via.placeholder.com/1920x750?text=Hello",
+                    order: 0
                 }, {
-                    image: "http://via.placeholder.com/1920x750?text=2"
+                    image: "http://via.placeholder.com/1920x750?text=There",
+                    order: 0
                 }, {
-                    image: "http://via.placeholder.com/1920x750?text=3"
+                    image: "http://via.placeholder.com/1920x750?text=General",
+                    order: 0
+                }, {
+                    image: "http://via.placeholder.com/1920x750?text=Kenobi",
+                    order: 0
                 },
-            ]
+            ],
+            isMoving: false
         };
+    }
+
+    componentDidMount() {
         this.initOrder();
     }
 
+
     initOrder() {
-        let items = this.state.items;
-        items.concat(items);
-        for (let i = 0; i < items.length; i++) {
-            items[i].order = i + 1;
+        let items = [];
+
+        for (let i = 0; i < 2; i++) {
+            for (let i = 0; i < this.state.items.length; i++) {
+                items.push({image: this.state.items[i].image, order: this.state.items[i].order});
+            }
         }
+
+        console.log(items);
+        for (let i = 0; i < items.length; i++) {
+            console.log(i + " -> " + (i + 1));
+            items[i].order = (i + 1) % items.length + 1;
+        }
+        this.setState({items: items});
     }
 
     moveLeft = () => {
-        let items = this.state.items;
-        for (let i = 0; i < items.length; i++) {
-            if (items[i].order === 1) {
-                items[i].order = items[i].order = items.length;
-            } else {
-                items[i].order = items[i].order - 1;
-            }
+        if (this.state.isMoving === false) {
+            let items = [...this.state.items];
+
+            this.setState({isMoving: "move-left"}, () => {
+                setTimeout(() => {
+                    for (let i = 0; i < items.length; i++) {
+                        if (items[i].order === 1) {
+                            items[i].order = items[i].order = items.length;
+                        } else {
+                            items[i].order = items[i].order - 1;
+                        }
+                    }
+
+                    this.setState({isMoving: false});
+                }, 300);
+            });
         }
-        this.setState({items: items});
     };
 
     moveRight = () => {
-        let items = this.state.items;
-        for (let i = 0; i < items.length; i++) {
-            items[i].order = items[i].order % items.length + 1;
+        if (this.state.isMoving === false) {
+            let items = [...this.state.items];
+
+            this.setState({isMoving: "move-right"}, () => {
+                setTimeout(() => {
+                    for (let i = 0; i < items.length; i++) {
+                        items[i].order = items[i].order % items.length + 1;
+                    }
+
+                    this.setState({isMoving: false});
+                }, 300);
+            });
         }
-        this.setState({items: items});
     };
 
     render() {
@@ -56,10 +92,10 @@ export default class Carousel extends React.Component {
                 <ul className="carousel-list">
                     {this.state.items.map((item, index) => {
                         return (
-                        <li style={{order: item.order}} key={index} className="carousel-list-item">
-                            <img className="carousel-list-item-img" src={item.image} alt=""/>
-                            <span className="carousel-list-item-play"><i className="fas fa-play fa-fw"/></span>
-                        </li>
+                            <li style={{order: item.order}} key={index} className={`carousel-list-item ${this.state.isMoving ? this.state.isMoving : ""}`}>
+                                <img className="carousel-list-item-img" src={item.image} alt=""/>
+                                <span className="carousel-list-item-play"><i className="fas fa-play fa-fw"/></span>
+                            </li>
                         );
                     })}
                 </ul>
