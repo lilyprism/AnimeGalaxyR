df740dda1fd4f299785b24b04c1fc7e7de089943
S4YG4 <telemovelsic@hotmail.com>
2019-07-04 12:53:18 +0100

    Added dotenv configuration file Fixed translations

--- AnimeGalaxy/AnimeGalaxy/settings.py
+++ AnimeGalaxy/AnimeGalaxy/settings.py
@@ -13,6 +13,10 @@ import datetime
 import os
 
 from django.utils.translation import ugettext_lazy as _
+from dotenv import load_dotenv
+
+# Load all dotenv configurations to system environment variables
+load_dotenv(verbose=True, override=True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
