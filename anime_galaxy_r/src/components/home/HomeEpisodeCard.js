52af184c31117ff242f7928ae8b0693bff5118ff
INatsuz <vascoraminhos@hotmail.com>
2019-07-09 11:11:01 +0100

    ~Fixed Link bug

--- anime_galaxy_r/src/components/home/HomeEpisodeCard.js
+++ anime_galaxy_r/src/components/home/HomeEpisodeCard.js
@@ -67,23 +67,31 @@ export default class HomeEpisodeCard extends React.Component {
                     <div className="card-image-container">
                         <img className="card-img" src={this.props.item.season.anime.image} alt="Episode"/>
                     </div>
-                    <div className="card-body">
-                        <div className="card-episode-top-container">
-                            <div className="card-episode-number-container">
-                                <div className="card-episode-number">
-                                    {`T${this.state.item.season.number} E${this.state.item.number}`}
-                                </div>
+                </Link>
+                <div className="card-body">
+                    <div className="card-episode-top-container">
+                        <div className="card-episode-number-container">
+                            <div className="card-episode-number">
+                                {`T${this.state.item.season.number} E${this.state.item.number}`}
                             </div>
                         </div>
-                        {
-                            this.props.is_logged_in ?
-                                <div className="card-episode-options">
-                                    <span onClick={this.favEpisode}><i className={`card-option fas fa-heart fa-fw ${this.state.item.favorite ? "active" : ""}`}/></span>
-                                    <span onClick={this.addEpisodeToWatchLater}><i className={`card-option fas fa-clock fa-fw ${this.state.item.watch_later ? "active" : ""}`}/></span>
-                                </div>
-                                :
-                                ""
-                        }
+                    </div>
+                    {
+                        this.props.is_logged_in ?
+                            <div className="card-episode-options">
+                                <span onClick={this.favEpisode}><i className={`card-option fas fa-heart fa-fw ${this.state.item.favorite ? "active" : ""}`}/></span>
+                                <span onClick={this.addEpisodeToWatchLater}><i className={`card-option fas fa-clock fa-fw ${this.state.item.watch_later ? "active" : ""}`}/></span>
+                            </div>
+                            :
+                            ""
+                    }
+
+                    <div className="filler">
+                        <Link to={`/v/${this.props.item.id}`}>
+                            <div className="h-100 w-100"/>
+                        </Link>
+                    </div>
+                    <Link to={`/v/${this.props.item.id}`}>
                         <div className="card-episode-info">
                             <div className="card-episode-name">
                                 {this.props.item.season.anime.name}
@@ -92,7 +100,9 @@ export default class HomeEpisodeCard extends React.Component {
                                 {time.fromNow()}
                             </div>
                         </div>
-                    </div>
+                    </Link>
+                </div>
+                <Link to={`/v/${this.props.item.id}`}>
                     <span className="card-play-icon"><i className="fas fa-play fa-fw"/></span>
                     <div className="card-gradient-overlay"/>
                 </Link>
