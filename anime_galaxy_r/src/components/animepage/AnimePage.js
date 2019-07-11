47c93a3aef2480c79da95b8a77fb9f50ad960361
INatsuz <vascoraminhos@hotmail.com>
2019-07-11 11:08:33 +0100

    +Added seasons to AnimePage ~Moved Rating to a separate component +Added placeholder information +Created voting function ~Fixed the trailer's Youtube player proportion

--- anime_galaxy_r/src/components/animepage/AnimePage.js
+++ anime_galaxy_r/src/components/animepage/AnimePage.js
@@ -5,13 +5,11 @@ import "./animepage.sass";
 import RequestUtilities from "./../../util/RequestUtilities";
 import ReactDOM from "react-dom";
 import Rating from "../rating/Rating";
+import App from "../App";
+import CardLayout from "../cardlayout/CardLayout";
+import {Link} from "react-router-dom";
 
 export default class AnimePage extends React.Component {
-
-    defaultColor = "#ffd700";
-    currentSelectionColor = "#ff3200";
-    overlapColor = "#ff3b44";
-    remainderColor = "#646464";
 
     constructor(props) {
         super(props);
@@ -20,8 +18,23 @@ export default class AnimePage extends React.Component {
             anime: {
                 name: "Fairy Tail",
                 description: "Ola",
-                image: "http://via.placeholder.com/250x350",
+                image: "http://via.placeholder.com/255x360",
                 thumbnail: "http://via.placeholder.com/1920x1080",
+                seasons: [
+                    {
+                        number: 1,
+                        name: "Season 1"
+                    }, {
+                        number: 2,
+                        name: "Season 2"
+                    }, {
+                        number: 3,
+                        name: "Season 3"
+                    }, {
+                        number: 4,
+                        name: "Season 4"
+                    }
+                ],
                 genres: [
                     {
                         name: "Ação",
@@ -38,12 +51,129 @@ export default class AnimePage extends React.Component {
                 rating: 48,
                 trailer: ""
             },
+            episodes: [
+                {
+                    id: -1,
+                    added: "2019-07-02T09:29:53.310375+01:00",
+                    favorite: false,
+                    number: "",
+                    watch_later: false,
+                    season: {
+                        number: "",
+                        anime: {
+                            id: 1,
+                            image: "/images/card_placeholder.webp",
+                            name: ""
+                        }
+                    }
+                }, {
+                    id: -2,
+                    added: "2019-07-02T09:29:53.310375+01:00",
+                    favorite: false,
+                    number: "",
+                    watch_later: false,
+                    season: {
+                        number: "",
+                        anime: {
+                            id: 1,
+                            image: "/images/card_placeholder.webp",
+                            name: ""
+                        }
+                    }
+                }, {
+                    id: -3,
+                    added: "2019-07-02T09:29:53.310375+01:00",
+                    favorite: false,
+                    number: "",
+                    watch_later: false,
+                    season: {
+                        number: "",
+                        anime: {
+                            id: 1,
+                            image: "/images/card_placeholder.webp",
+                            name: ""
+                        }
+                    }
+                }, {
+                    id: -4,
+                    added: "2019-07-02T09:29:53.310375+01:00",
+                    favorite: false,
+                    number: "",
+                    watch_later: false,
+                    season: {
+                        number: "",
+                        anime: {
+                            id: 1,
+                            image: "/images/card_placeholder.webp",
+                            name: ""
+                        }
+                    }
+                }, {
+                    id: -5,
+                    added: "2019-07-02T09:29:53.310375+01:00",
+                    favorite: false,
+                    number: "",
+                    watch_later: false,
+                    season: {
+                        number: "",
+                        anime: {
+                            id: 1,
+                            image: "/images/card_placeholder.webp",
+                            name: ""
+                        }
+                    }
+                }, {
+                    id: -6,
+                    added: "2019-07-02T09:29:53.310375+01:00",
+                    favorite: false,
+                    number: "",
+                    watch_later: false,
+                    season: {
+                        number: "",
+                        anime: {
+                            id: 1,
+                            image: "/images/card_placeholder.webp",
+                            name: ""
+                        }
+                    }
+                }, {
+                    id: -7,
+                    added: "2019-07-02T09:29:53.310375+01:00",
+                    favorite: false,
+                    number: "",
+                    watch_later: false,
+                    season: {
+                        number: "",
+                        anime: {
+                            id: 1,
+                            image: "/images/card_placeholder.webp",
+                            name: ""
+                        }
+                    }
+                }, {
+                    id: -8,
+                    added: "2019-07-02T09:29:53.310375+01:00",
+                    favorite: false,
+                    number: "",
+                    watch_later: false,
+                    season: {
+                        number: "",
+                        anime: {
+                            id: -1,
+                            image: "/images/card_placeholder.webp",
+                            name: ""
+                        }
+                    }
+                }
+            ],
+            current_season: 0,
+            current_page: 1
         };
-        // this.getAnimeDetails();
+        this.getAnimeDetails();
     }
 
     getAnimeDetails = () => {
-        RequestUtilities.sendGetRequest(`anime/${this.props.match.params.id}`, false).then(res => {
+        RequestUtilities.sendGetRequest(`anime/${this.props.match.params.id}`, App.isLoggedIn()).then(res => {
             this.setState({anime: res.data});
             console.log(res.data);
         });
@@ -67,37 +197,39 @@ export default class AnimePage extends React.Component {
         }
     };
 
+    sendVote = rating => {
+        if (this.props.is_logged_in) {
+            RequestUtilities.sendPostRequest(`anime/${this.state.anime.id}/vote`, {rating: rating}, true).then(res => {
+                console.log(res.data);
+                let animeRating = {...this.state.anime.rating};
+                let anime = {...this.state.anime};
+
+                if (anime.user_rating != null) {
+                    animeRating.number -= anime.user_rating / animeRating.votes;
+                } else {
+                    animeRating.votes += 1;
+                }
+                animeRating.number = animeRating.number + rating / animeRating.votes;
+
+                anime.rating = animeRating;
+                anime.user_rating = rating;
+                this.setState({anime: anime});
+            }).catch(err => {
+                console.log(err.response);
+            });
+        }
+    };
+
     render() {
         if (this.state.anime !== null) {
             let description = this.state.anime.description;
-
-            let stars_html = [];
-            for (let i = 0; i < 5; i++) {
-                stars_html.push(
-                    <i className="fas fa-star star" key={i}/>
-                );
-            }
-
-            let style = {};
-            if (this.state.ratingMode === 0) {
-                console.log("0");
-                style.backgroundImage = `linear-gradient(to right, ${this.defaultColor} 0% ${this.state.anime.rating}%, ${this.remainderColor} 0% 100%)`;
-                console.log(style);
-            } else {
-                console.log("1");
-                if (this.state.mouseOverRating > this.state.anime.rating) {
-                    style.backgroundImage = `linear-gradient(to right, ${this.overlapColor} 0% ${this.state.anime.rating}%, ${this.currentSelectionColor} ${this.state.anime.rating}% ${this.state.mouseOverRating}%, ${this.remainderColor} 0% 100%)`;
-                } else {
-                    style.backgroundImage = `linear-gradient(to right, ${this.overlapColor} 0% ${this.state.mouseOverRating}%, ${this.defaultColor} ${this.state.mouseOverRating}% ${this.state.anime.rating}%, ${this.remainderColor} 0% 100%)`;
-                }
-            }
 
             return (
                 <div className="anime-page">
                     <div className="trailer-container">
                         <div className="trailer-container-overlay" onClick={this.handleThumbnailOverlayClick}/>
                         <div className="trailer">
-                            <iframe id="trailer-player" src={``} frameBorder="0" autoPlay/>
+                            <iframe id="trailer-player" src={``} title="Anime Trailer" frameBorder="0" autoPlay/>
                         </div>
                     </div>
 
@@ -107,7 +239,7 @@ export default class AnimePage extends React.Component {
                             <div className="anime-details">
                                 <div className="anime-thumbnail" style={{backgroundImage: `url(${this.state.anime.thumbnail})`}}>
                                     <div className="breadcrumbs">
-                                        <p>Home <i className="fas fa-chevron-right"/> Anime <i className="fas fa-chevron-right"/> {this.state.anime.name}</p>
+                                        <p><Link to={"/"}>Home</Link> > <Link to={"/anime"}>Anime</Link> > {this.state.anime.name}</p>
                                     </div>
                                     <div className="thumbnail-play-btn" onClick={this.handleThumbnailPlayClick}>
                                         <i className="fas fa-play"/>
@@ -117,7 +249,7 @@ export default class AnimePage extends React.Component {
                                     <div className="anime-details-left">
                                         <div className="anime-image-description">
                                             <div className="anime-image-container">
-                                                <img className="anime-image" src={this.state.anime.image} alt="Anime Image"/>
+                                                <img className="anime-image" src={this.state.anime.image} alt="Anime"/>
                                             </div>
                                             <div className="anime-description-title">
                                                 <h2 className="anime-title">
@@ -135,13 +267,38 @@ export default class AnimePage extends React.Component {
                                     <div className="anime-details-right">
                                         <div className="anime-details-rating">
                                             <p className="font-size-20px text-center rating-title">Rating</p>
-                                            <p className="rating-vote-number">(Based on 300 votes)</p>
+                                            <p className="rating-vote-number">(Based on {this.state.anime.rating.votes} votes)</p>
                                             <div className="rating-container">
-                                                <Rating rating={this.state.anime.rating} intervals={10} stars={5}/>
+                                                <Rating readOnly={!this.props.is_logged_in} rating={this.state.anime.rating.number} newRating={this.state.anime.user_rating} intervals={10} stars={5} onClick={this.sendVote}/>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            <div className="spacer"/>
+                        </div>
+                    </div>
+                    <div className="border-bottom-red"/>
+                    <div className="gradient-container">
+                        <div className="breakpoint-container">
+                            <h2 className="bg-title">
+                                <div className="pless-title">
+                                    <span>Lista de Episódios</span>
+                                </div>
+                            </h2>
+                            <div className="spacer"/>
+                            <div className="seasons">
+                                {this.state.anime.seasons.map((season, index) => {
+                                    return (
+                                        <div className={`season${index === this.state.current_season ? " active" : ""}`}>
+                                            <span className="season-name">{season.name}</span>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                            <div className="spacer"/>
+                            <div className="anime-episodes-container">
+                                <CardLayout items={this.state.episodes} type={5} xl={4} l={4} md={2} sm={2} is_logged_in={this.props.is_logged_in} animate={false}/>
                             </div>
                             <div className="spacer"/>
                         </div>
