a7041a4627c175528029d9413cdec39cc5593d79
INatsuz <vascoraminhos@hotmail.com>
2019-07-11 10:58:29 +0100

    +Added updatibility through prop changes

--- anime_galaxy_r/src/components/rating/Rating.js
+++ anime_galaxy_r/src/components/rating/Rating.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import RequestUtilities from "../../util/RequestUtilities";
+
+import "./rating.sass";
 
 export default class Rating extends React.Component {
 
@@ -8,8 +9,9 @@ export default class Rating extends React.Component {
 
         this.state = {
             rating: this.props.rating != null ? this.props.rating : 0,
-            ratingInterval: 100 / this.props.intervals, // 100 / <Number of intervals you want>
+            newRating: this.props.newRating,
             mouseOverRating: 0,
+            ratingInterval: 100 / this.props.intervals, // 100 / <Number of intervals you want>
             isMouseOver: false,
 
             starColor: this.props.starColor != null ? this.props.starColor : "#ffd700",
@@ -17,6 +19,47 @@ export default class Rating extends React.Component {
             overlapColor: this.props.overlapColor != null ? this.props.overlapColor : "#ff3b44",
             remainderColor: this.props.remainderColor != null ? this.props.remainderColor : "#646464",
         };
+    }
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        if (nextProps !== this.props) {
+            if (this.props.rating !== nextProps.rating) {
+                this.setState({
+                    rating: nextProps.rating != null ? nextProps.rating : 0,
+                });
+            }
+            if (this.props.newRating !== nextProps.newRating) {
+                this.setState({
+                    newRating: nextProps.newRating
+                });
+            }
+            if (this.props.intervals !== nextProps.intervals) {
+                this.setState({
+                    ratingInterval: 100 / nextProps.intervals, // 100 / <Number of intervals you want>
+                });
+            }
+
+            if (this.props.starColor !== nextProps.starColor) {
+                this.setState({
+                    starColor: nextProps.starColor != null ? nextProps.starColor : "#ffd700"
+                });
+            }
+            if (this.props.hoverColor !== nextProps.hoverColor) {
+                this.setState({
+                    hoverColor: nextProps.hoverColor != null ? nextProps.hoverColor : "#ff3200"
+                });
+            }
+            if (this.props.overlapColor !== nextProps.overlapColor) {
+                this.setState({
+                    overlapColor: nextProps.overlapColor != null ? nextProps.overlapColor : "#ff3b44"
+                });
+            }
+            if (this.props.remainderColor !== nextProps.remainderColor) {
+                this.setState({
+                    remainderColor: nextProps.remainderColor != null ? nextProps.remainderColor : "#646464"
+                });
+            }
+        }
     }
 
     handleMouseMoveRating = event => {
@@ -34,17 +77,15 @@ export default class Rating extends React.Component {
         this.setState({isMouseOver: false});
     };
 
-    // handleClickRating = () => {
-    //     let = {...this.state};
-    //    .rating = this.state.mouseOverRating;
-    //     this.setState(:});
-    //     RequestUtilities.sendPutRequest(/rating", {rating: this.state.mouseOverRating}, true).then(res => {
-    //         console.log("Hello there");
-    //     }).catch(err => {
-    //         console.log(err.response);
-    //     });
-    // };
+    handleOnClick = () => {
+        this.setState({newRating: this.state.mouseOverRating}, () => {
+            if (this.props.onClick != null) {
+                console.log(this.state.newRating);
+                this.props.onClick(this.state.newRating);
+            }
+        });
 
+    };
 
     render() {
         let style = {};
@@ -57,23 +98,38 @@ export default class Rating extends React.Component {
         }
 
         if (!this.state.isMouseOver) {
-            console.log("0");
-            style.backgroundImage = `linear-gradient(to right, ${this.state.starColor} 0% ${this.state.rating}%, ${this.state.remainderColor} 0% 100%)`;
-            console.log(style);
+            if (this.state.newRating != null) {
+                if (this.state.newRating > this.state.rating) {
+                    style.backgroundImage = `linear-gradient(to right, ${this.state.starColor} 0% ${this.state.rating}%, ${this.state.hoverColor} ${this.state.rating}% ${this.state.newRating}%, ${this.state.remainderColor} 0% 100%)`;
+                    // style.backgroundImage = `linear-gradient(to right, ${this.state.overlapColor} 0% ${this.state.rating}%, ${this.state.hoverColor} ${this.state.rating}% ${this.state.newRating}%, ${this.state.remainderColor} 0% 100%)`;
+                } else {
+                    style.backgroundImage = `linear-gradient(to right, ${this.state.overlapColor} 0% ${this.state.newRating}%, ${this.state.starColor} ${this.state.newRating}% ${this.state.rating}%, ${this.state.remainderColor} 0% 100%)`;
+                }
+            } else {
+                style.backgroundImage = `linear-gradient(to right, ${this.state.starColor} 0% ${this.state.rating}%, ${this.state.remainderColor} 0% 100%)`;
+            }
         } else {
-            console.log("1");
             if (this.state.mouseOverRating > this.state.rating) {
+                // style.backgroundImage = `linear-gradient(to right, ${this.state.starColor} 0% ${this.state.rating}%, ${this.state.hoverColor} ${this.state.rating}% ${this.state.mouseOverRating}%, ${this.state.remainderColor} 0% 100%)`;
                 style.backgroundImage = `linear-gradient(to right, ${this.state.overlapColor} 0% ${this.state.rating}%, ${this.state.hoverColor} ${this.state.rating}% ${this.state.mouseOverRating}%, ${this.state.remainderColor} 0% 100%)`;
             } else {
                 style.backgroundImage = `linear-gradient(to right, ${this.state.overlapColor} 0% ${this.state.mouseOverRating}%, ${this.state.starColor} ${this.state.mouseOverRating}% ${this.state.rating}%, ${this.state.remainderColor} 0% 100%)`;
             }
         }
 
-        return (
-            <div className="rating" style={style} title={this.state.mouseOverRating} onMouseEnter={this.handleMouseEnterRating} onMouseMove={this.handleMouseMoveRating} onMouseLeave={this.handleMouseLeaveRating} onClick={this.props.onClick} onTouchMove={this.handleMouseMoveRating}>
-                {stars_html}
-            </div>
-        );
+        if (this.props.readOnly) {
+            return (
+                <div className="rating" style={style}>
+                    {stars_html}
+                </div>
+            );
+        } else {
+            return (
+                <div className="rating" style={style} title={this.state.mouseOverRating} onMouseEnter={this.handleMouseEnterRating} onMouseMove={this.handleMouseMoveRating} onMouseLeave={this.handleMouseLeaveRating} onClick={this.handleOnClick} onTouchMove={this.handleMouseMoveRating}>
+                    {stars_html}
+                </div>
+            );
+        }
     }
 
 };
